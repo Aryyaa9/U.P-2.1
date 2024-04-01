bfb1efe504be2e29b01a6b68360555837ae4dda8
Aryyaa9 <153473157+Aryyaa9@users.noreply.github.com>
2024-04-01 11:21:59 +0700

    Add files via upload

---
+++ task_3/task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{ static bool ContainsDuplicate(int[] nums)
+    {
+        Array.Sort(nums);
+        
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+                return true;
+        }
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = { 2, 2, 40, 8, 11 };
+        
+        Console.WriteLine(ContainsDuplicate(nums));
+    }
+}
