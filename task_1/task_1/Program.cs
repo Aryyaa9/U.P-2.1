9aa3b8f06a05203a4c28b71a02ee8df42b012bd8
Aryyaa9 <153473157+Aryyaa9@users.noreply.github.com>
2024-04-01 10:35:09 +0700

    Add files via upload

---
+++ task_1/task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static int CountJewels(string J, string S)
+    {
+        int count = 0;
+        foreach (char c in S)
+        {
+            if (J.Contains(c))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку J (драгоценности):");
+        string J = Console.ReadLine();
+
+        Console.WriteLine("Введите строку S (камни):");
+        string S = Console.ReadLine();
+
+        int result = CountJewels(J, S);
+        Console.WriteLine($"Вывод: {result}");
+    }
+}
